e17336f35d5611216ad9570c2b891e1f2f8091ca
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-15 12:46:17 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def covariance_spin( spins, r ) :
+  # Your code goes here
+  
+  return 
+
+spins1 = [1,-1,1,1,-1,1,1,1,-1,-1]
+print( covariance_spin( spins1, 1 ), covariance_spin( spins1, 2 ), covariance_spin( spins1, 3 ) )
+spins2 = [-1,1,1,1,-1,-1,1,1,1,-1]
+print( covariance_spin( spins2, 2 ), covariance_spin( spins2, 2 ), covariance_spin( spins2, 3 ) )
