79c44c433de7d905ad09e504c319825e2117bf01
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-16 14:51:43 +0000

    The answer

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def covariance_spin( spins, r ) :
   # Your code goes here
-  
-  return 
+  avspin, corr = sum(spins)/len(spins), 0 
+  for i in range(len(spins)) : corr = corr + (spins[i]-avspin)*(spins[(i+r)%len(spins)]-avspin)
+  return corr / len(spins)
 
 spins1 = [1,-1,1,1,-1,1,1,1,-1,-1]
 print( covariance_spin( spins1, 1 ), covariance_spin( spins1, 2 ), covariance_spin( spins1, 3 ) )
